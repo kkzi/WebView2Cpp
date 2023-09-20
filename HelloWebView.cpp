f48d2264535b90985f5de4ed91b32fe647a245bc
gx <xuan.guo@atomdatatech.com>
2023-09-20 12:26:52 +0800

    use_webview2.hpp

--- HelloWebView.cpp
+++ HelloWebView.cpp
@@ -4,16 +4,14 @@
 #include <stdlib.h>
 #include <string>
 #include <tchar.h>
-#include <wrl.h>
-#include <wil/com.h>
+#include <simple/use_webview2.hpp>
 // <IncludeHeader>
 // include WebView2 header
-#include <WebView2/WebView2.h>
 // </IncludeHeader>
 
-using namespace Microsoft::WRL;
-
 // Global variables
+
+static std::unique_ptr<WebViewAgent> agent_;
 
 // The main window class name.
 static TCHAR szWindowClass[] = _T("DesktopApp");
@@ -25,12 +23,6 @@ HINSTANCE hInst;
 
 // Forward declarations of functions included in this code module:
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
-// Pointer to WebViewController
-static wil::com_ptr<ICoreWebView2Controller> webviewController;
-
-// Pointer to WebView window
-static wil::com_ptr<ICoreWebView2> webview;
 
 int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In_ LPSTR     lpCmdLine, _In_ int       nCmdShow)
 {
@@ -103,91 +95,9 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In
     // <-- WebView2 sample code starts here -->
     // Step 3 - Create a single WebView within the parent window
     // Locate the browser and set up the environment for WebView
-    CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr, Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
-            [hWnd](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
-
-                // Create a CoreWebView2Controller and get the associated CoreWebView2 whose parent is the main window hWnd
-                env->CreateCoreWebView2Controller(hWnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
-                    [hWnd](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
-                        if (controller != nullptr) {
-                            webviewController = controller;
-                            webviewController->get_CoreWebView2(&webview);
-                        }
-
-                        // Add a few settings for the webview
-                        // The demo step is redundant since the values are the default settings
-                        wil::com_ptr<ICoreWebView2Settings> settings;
-                        webview->get_Settings(&settings);
-                        settings->put_IsScriptEnabled(TRUE);
-                        settings->put_AreDefaultScriptDialogsEnabled(TRUE);
-                        settings->put_IsWebMessageEnabled(TRUE);
-
-                        // Resize WebView to fit the bounds of the parent window
-                        RECT bounds;
-                        GetClientRect(hWnd, &bounds);
-                        webviewController->put_Bounds(bounds);
-
-                        // Schedule an async task to navigate to Bing
-                        webview->Navigate(L"https://www.bing.com/");
-
-                        // <NavigationEvents>
-                        // Step 4 - Navigation events
-                        // register an ICoreWebView2NavigationStartingEventHandler to cancel any non-https navigation
-                        EventRegistrationToken token;
-                        webview->add_NavigationStarting(Callback<ICoreWebView2NavigationStartingEventHandler>(
-                            [](ICoreWebView2* webview, ICoreWebView2NavigationStartingEventArgs* args) -> HRESULT {
-                                wil::unique_cotaskmem_string uri;
-                                args->get_Uri(&uri);
-                                std::wstring source(uri.get());
-                                if (source.substr(0, 5) != L"https") {
-                                    args->put_Cancel(true);
-                                }
-                                return S_OK;
-                            }).Get(), &token);
-                        // </NavigationEvents>
-
-                        // <Scripting>
-                        // Step 5 - Scripting
-                        // Schedule an async task to add initialization script that freezes the Object object
-                        webview->AddScriptToExecuteOnDocumentCreated(L"Object.freeze(Object);", nullptr);
-                        // Schedule an async task to get the document URL
-                        webview->ExecuteScript(L"window.document.URL;", Callback<ICoreWebView2ExecuteScriptCompletedHandler>(
-                            [](HRESULT errorCode, LPCWSTR resultObjectAsJson) -> HRESULT {
-                                LPCWSTR URL = resultObjectAsJson;
-                                //doSomethingWithURL(URL);
-                                return S_OK;
-                            }).Get());
-                        // </Scripting>
-
-                        // <CommunicationHostWeb>
-                        // Step 6 - Communication between host and web content
-                        // Set an event handler for the host to return received message back to the web content
-                        webview->add_WebMessageReceived(Callback<ICoreWebView2WebMessageReceivedEventHandler>(
-                            [](ICoreWebView2* webview, ICoreWebView2WebMessageReceivedEventArgs* args) -> HRESULT {
-                                wil::unique_cotaskmem_string message;
-                                args->TryGetWebMessageAsString(&message);
-                                // processMessage(&message);
-                                webview->PostWebMessageAsString(message.get());
-                                return S_OK;
-                            }).Get(), &token);
-
-                        // Schedule an async task to add initialization script that
-                        // 1) Add an listener to print message from the host
-                        // 2) Post document URL to the host
-                        webview->AddScriptToExecuteOnDocumentCreated(
-                            L"window.chrome.webview.addEventListener(\'message\', event => alert(event.data));" \
-                            L"window.chrome.webview.postMessage(window.document.URL);",
-                            nullptr);
-                        // </CommunicationHostWeb>
-
-                        return S_OK;
-                    }).Get());
-                return S_OK;
-            }).Get());
-
-
-
     // <-- WebView2 sample code ends here -->
+    //view = std::make_unique<wv::WebView>(hWnd, L"https://bing.com");
+    agent_ = std::make_unique<WebViewAgent>(hWnd, L"http://localhost");
 
     // Main message loop:
     MSG msg;
@@ -212,10 +122,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     switch (message)
     {
     case WM_SIZE:
-        if (webviewController != nullptr) {
-            RECT bounds;
-            GetClientRect(hWnd, &bounds);
-            webviewController->put_Bounds(bounds);
+        if (agent_ != nullptr) {
+            agent_->update_bounds();
         };
         break;
     case WM_DESTROY:
